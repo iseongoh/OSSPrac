4b4390d39421feea9680f3b78ce3c048a1bc6da8
iseongoh <lso04177@chosun.ac.kr>
2026-04-09 14:15:10 +0900

    실습2

--- subject5/main.c
+++ subject5/main.c
@@ -3,18 +3,22 @@
 int main()
 {
     char name[20];
-    int grade;
+    int gradenum;
     char class[50];
+    int grade;
     printf("이름을 입력하세요: ");
     scanf("%s", &name);
     printf("학번을 입력하세요: ");
-    scanf("%d", &grade);
+    scanf("%d", &gradenum);
     printf("학과를 입력하세요: ");
     scanf("%s", &class);
-
+    printf("학년을 입력하세요: ");
+    scanf("%d", &grade);    
+    
     printf("\n<출력>\n");
     printf("이름: %s\n", name);
-    printf("학번: %d\n", grade);
+    printf("학번: %d\n", gradenum);
     printf("학과: %s\n", class);
+    printf("학년: %d\n", grade);
     return 0;
 }
